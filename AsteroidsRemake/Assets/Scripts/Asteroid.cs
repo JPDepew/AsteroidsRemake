02376d58acb8288ee3c3ca9d568504cca64c2b54
John Paul Depew <jpdepewjp@gmail.com>
2018-09-16 19:27:04 -0400

    Created project + Asteroid.cs and SceneManager.cs
    Just basic random movement in a direction for the asteroid.

---
+++ AsteroidsRemake/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float speed = 2f;
+    Vector2 direction;
+
+    void Start()
+    {
+        direction = Random.insideUnitCircle.normalized;
+    }
+
+    void Update()
+    {
+        transform.position = (Vector2)transform.position + direction * speed * Time.deltaTime;
+    }
+}
---
+++ AsteroidsRemake/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    public GameObject asteroid;
+    public float numberOfAsteroids;
+
+	void Start () {
+        Instantiate(asteroid);
+	}
+	
+	void Update () {
+		
+	}
+}
